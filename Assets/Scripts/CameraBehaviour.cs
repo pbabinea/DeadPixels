b61e710f60367589dfcb35c3c964ee98bb1da83e
Phoebe Babineau <46791979+pbabinea@users.noreply.github.com>
2020-10-23 17:31:06 -0500

    CameraFollow
    Camera follows player, but stays within map bounds

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Transform player;
+    public float maxX;
+    public float minX;
+    public float maxY;
+    public float minY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //camera centers on player, but stops on edge of bounds
+        Vector3 currentPosition = transform.position;
+        Vector3 targetPosition = player.position;
+
+        //don't move
+        if ((targetPosition.x > maxX / 2 || targetPosition.x < minX / 2) && (targetPosition.y > maxY / 2 || targetPosition.y < minY / 2))
+            transform.position = currentPosition;
+        //only change y position 
+        else if (targetPosition.x > maxX / 2 || targetPosition.x < minX / 2)
+            transform.position = new Vector3(currentPosition.x, targetPosition.y, currentPosition.z);
+        //only change x position
+        else if (targetPosition.y > maxY / 2 || targetPosition.y < minY / 2)
+            transform.position = new Vector3(targetPosition.x, currentPosition.y, currentPosition.z);
+        //center player
+        else transform.position = new Vector3(targetPosition.x, targetPosition.y, currentPosition.z);
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //L/R movement
+        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+        rb.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, rb.velocity.y);
+
+        //Up/Down movement
+        transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
+        rb.velocity = new Vector2(verticalInput * speed * Time.deltaTime, rb.velocity.x);
+    }
+}
