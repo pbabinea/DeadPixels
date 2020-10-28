707c310c338f46591fc193f5589b76472f99c8ac
Phoebe Babineau <46791979+pbabinea@users.noreply.github.com>
2020-10-28 10:37:00 -0500

    added colliders downstairs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    //private Rigidbody2D rb;
     public float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        //rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,10 +21,10 @@ public class PlayerController : MonoBehaviour
 
         //L/R movement
         transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
-        rb.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, rb.velocity.y);
+        //rb.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, rb.velocity.y);
 
         //Up/Down movement
         transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
-        rb.velocity = new Vector2(verticalInput * speed * Time.deltaTime, rb.velocity.x);
+        //rb.velocity = new Vector2(verticalInput * speed * Time.deltaTime, rb.velocity.x);
     }
 }
