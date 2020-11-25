ef375de44010d6ef1d142d05ac97a9c6f56b6615
Phoebe Babineau <46791979+pbabinea@users.noreply.github.com>
2020-11-25 15:58:18 -0600

    changed to switch statements
    to make SceneTransition more readable

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -20,66 +20,68 @@ public class SceneTransition : MonoBehaviour
 
     public void LoadNextScene()
     {
+        //set spawn location based on exit/entry points of a scene
         string currentScene = SceneManager.GetActiveScene().name;
-        if (currentScene == "DemoMenu")
+        switch (currentScene)
         {
-            if (nextScene == "Bedroom") SetSpawn(6.34f, 0.31f, 1f);
+            case "DemoMenu":
+                if (nextScene == "Bedroom") SetSpawn(6.34f, 0.31f, 1f);
+                break;
+            case "PlayerHouse":
+                if (nextScene == "Bedroom") SetSpawn(-7.97f, -2.38f, 1f);
+                if (nextScene == "Town") SetSpawn(-3.44f, -1.24f, 1f);
+                break;
+            case "Bedroom":
+                if (nextScene == "PlayerHouse") SetSpawn(-2.039f, 2.418f, 1f);
+                break;
+            case "Town":
+                if (nextScene == "PlayerHouse") SetSpawn(-0.53f, -2.04f, 1f);
+                if (nextScene == "Library Puzzle 1")
+                {
+                    //skip Library Puzzle 1 if Library Puzzle 2 has already been solved (i.e. you have the key already)
+                    if (FindObjectOfType<GlobalControl>().hasLibKey) nextScene = "Library Puzzle 2";
+                    SetSpawn(-2.02f, -4.19f, 1f);
+                }
+                if (nextScene == "House1") SetSpawn(0.99f, -3.38f, 1f);
+                break;
+            case "Library Puzzle 1":
+            case "Library Puzzle 2":
+                if (nextScene == "Town") SetSpawn(18.95f, 9.14f, 1f);
+                if (nextScene == "Library Puzzle 2")
+                {
+                    // make sure the block stays put between library puzzle 1 and 2
+                    Vector3 blockPos = FindObjectOfType<PuzzleBlock>().transform.position;
+                    PlayerPrefs.SetFloat("blockX", blockPos.x);
+                    PlayerPrefs.SetFloat("blockY", blockPos.y);
+                    PlayerPrefs.SetFloat("blockZ", blockPos.z);
+                    SetSpawn(4.17f, -2.75f, 1f);
+                }
+                if (nextScene == "Library Puzzle 3") SetSpawn(4.51f, 3.52f, 1f);
+                break;
+            case "House1":
+                if (nextScene == "Town") SetSpawn(25.96f, -1.51f, 1f);
+                if (nextScene == "Basement1") SetSpawn(9.03f, -1.62f, 1f);
+                break;
+            case "Basement1":
+                if (nextScene == "House1")
+                {
+                    SetSpawn(15.5f, 3.92f, 1f);
+                    if (GlobalControl.Instance.hasBasButton) nextScene = "DemoEnding";
+                }
+                break;
+            case "Library Puzzle 3":
+                if (nextScene == "Library Puzzle 2") SetSpawn(4.17f, -2.75f, 1f);
+                break;
+            default:
+                break;
         }
-        if (currentScene == "PlayerHouse")
-        {
-            if (nextScene == "Bedroom") SetSpawn(-7.97f, -2.38f, 1f);
-            if (nextScene == "Town") SetSpawn(-3.44f, -1.24f, 1f);
-        }
-        if (currentScene == "Bedroom")
-        {
-            if (nextScene == "PlayerHouse") SetSpawn(-2.039f, 2.418f, 1f);
-        }
-        if (currentScene == "Town")
-        {
-            if (nextScene == "PlayerHouse") SetSpawn(-0.53f, -2.04f, 1f);
-            if (nextScene == "Library Puzzle 1")
-            {
-                if (FindObjectOfType<GlobalControl>().hasLibKey) nextScene = "Library Puzzle 2";
-                SetSpawn(-2.02f, -4.19f, 1f);
-            }
-            if (nextScene == "House1") SetSpawn(0.99f, -3.38f, 1f);
-        }
-        if (currentScene == "Library Puzzle 1" || currentScene == "Library Puzzle 2")
-        {
-            if (nextScene == "Town") SetSpawn(18.95f, 9.14f, 1f);
-            if (nextScene == "Library Puzzle 2")
-            {
-                // make sure the block stays put between library puzzle 1 and 2
-                Vector3 blockPos = FindObjectOfType<PuzzleBlock>().transform.position;
-                PlayerPrefs.SetFloat("blockX", blockPos.x);
-                PlayerPrefs.SetFloat("blockY", blockPos.y);
-                PlayerPrefs.SetFloat("blockZ", blockPos.z);
-                SetSpawn(4.17f, -2.75f, 1f);
-            }
-            if (nextScene == "Library Puzzle 3") SetSpawn(4.51f, 3.52f, 1f);
-        }
-        if (currentScene == "House1")
-        {
-            if (nextScene == "Town") SetSpawn(25.96f, -1.51f, 1f);
-            if (nextScene == "Basement1") SetSpawn(9.03f, -1.62f, 1f);
-        }
-        if (currentScene == "Basement1")
-        {
-            //please
-            if (nextScene == "House1")
-            {
-                SetSpawn(15.5f, 3.92f, 1f);
-                if (GlobalControl.Instance.hasBasButton) nextScene = "DemoEnding";
-            }
-        }
-        if (currentScene == "Library Puzzle 3")
-        {
-            if (nextScene == "Library Puzzle 2") SetSpawn(4.17f, -2.75f, 1f);
-        }
+        //set a checkpoint battery upon loading new scene
         if (currentScene != "DemoMenu") GlobalControl.Instance.checkpointBattery = GlobalControl.Instance.currentBattery;
+        //load next scene
         SceneManager.LoadScene(nextScene);
     }
 
+    //set the spawn point of the player
     private void SetSpawn(float x, float y, float z)
     {
         PlayerPrefs.SetFloat("PlayerSpawnX", x);
