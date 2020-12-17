da4b8fdfbf7fce92be26ed3f592290b079f4a80f
Phoebe Babineau <46791979+pbabinea@users.noreply.github.com>
2020-12-17 01:04:24 -0600

    Hint System

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ButtonManager : MonoBehaviour
 {
     public Text sliderText;
+    public Text hintText;
 
     private void Update()
     {
@@ -25,5 +26,14 @@ public class ButtonManager : MonoBehaviour
     {
         GlobalControl.Instance.backlight = val;
         GlobalControl.Instance.setBacklight();
+    }
+
+    public void GiveHint()
+    {
+        if (GlobalControl.Instance.hasMazeButton) hintText.text = "Didn't Sock say he'd be waiting in my room once I got all the buttons?";
+        else if (GlobalControl.Instance.hasArtButton) hintText.text = "I wonder if anymore windows have lit up?";
+        else if (GlobalControl.Instance.hasBasButton) hintText.text = "It seemed like that house's window lit up after I got the button in the library...";
+        else if (GlobalControl.Instance.hasLibButton) hintText.text = "I wonder if anything has changed around town?";
+        else hintText.text = "I should go to the library. It should be in the northeast part of town.";
     }
 }
